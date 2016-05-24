5b8f8859d1d6907a96f0581bc32203776acb815b
Martin Bella <martin.bella@deltadna.com>
2016-05-24 16:18:43 +0100

    added support for session notifications [ptl-2161]

---
+++ library/src/test/java/com/deltadna/android/sdk/DDNATest.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2016 deltaDNA Ltd. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deltadna.android.sdk
+
+import android.os.Build
+import com.deltadna.android.sdk.helpers.Settings
+import com.deltadna.android.sdk.listeners.SessionListener
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockito_kotlin.*
+
+@RunWith(RobolectricGradleTestRunner::class)
+@Config(constants = BuildConfig::class,
+        sdk = intArrayOf(Build.VERSION_CODES.LOLLIPOP))
+class DDNATest {
+    
+    private var uut = createUut()
+    
+    @Before
+    fun before() {
+        uut = createUut()
+    }
+    
+    @Test
+    fun sessionChanging() {
+        val s1 = uut.newSession().sessionId
+        assertThat(s1).isNotEmpty()
+        
+        val s2 = uut.newSession().sessionId
+        assertThat(s2).isNotEmpty()
+        assertThat(s2).isNotEqualTo(s1)
+    }
+    
+    @Test
+    fun sessionNotifications() {
+        with(mock<SessionListener>()) {
+            uut.register(this)
+            uut.newSession()
+            uut.newSession()
+            
+            verify(this, times(2)).onSessionUpdated()
+            
+            uut.unregister(this)
+            uut.newSession()
+            
+            verifyNoMoreInteractions(this)
+        }
+    }
+    
+    private fun createUut() = DDNA(
+            RuntimeEnvironment.application,
+            "environmentKey",
+            "collectUrl",
+            "engageUrl",
+            Settings(),
+            null,
+            null,
+            null)
+}
