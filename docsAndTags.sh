0e020535a527f1dfdcbbc7e3343b40c05ab3e211
Tim Hull <tim.hull@unity3d.com>
2020-11-12 15:41:10 +0000

    DDSDK Version Bump to 4.13.2

--- docsAndTags.sh
+++ docsAndTags.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 version=$(sed '/^\#/d' gradle.properties | grep 'VERSION_NAME' | tail -n 1 | cut -d "=" -f2-)
 
+# This user is just a placeholder - no public commit should be attributed to them
+git config user.name "DeltaDNA"
+git config user.email "placeholder@example.com"
+
 git tag $version
 
 git push origin $version
@@ -14,4 +18,3 @@ git remote add github git@github.com:deltaDNA/android-sdk.git
 git push -f github HEAD
 git push github $version
 
-./scripts/publish-javadocs
